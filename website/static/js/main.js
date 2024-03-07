eb48f80aba5352c6d04dafbef9d5d353c5c47c2e
คีตศิลป์ คงสี <keetasin01596@gmail.com>
2024-03-07 14:53:54 +0700

    Portfolio Section

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -163,21 +163,20 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+    let portfolioContainer = select('#portfolio-grid');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        itemSelector: '.item',
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#filters a', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#filters a', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+          el.classList.remove('active');
         });
-        this.classList.add('filter-active');
+        this.classList.add('active');
 
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
@@ -211,34 +210,6 @@
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
-      }
     }
   });
 
